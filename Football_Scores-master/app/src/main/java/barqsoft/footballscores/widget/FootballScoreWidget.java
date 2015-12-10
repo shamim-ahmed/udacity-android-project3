88d5fd3666c9d8b2b9a007e5ba114dcc3610159c
Shamim Ahmed <shamim.buet.99@gmail.com>
2015-12-10 22:14:32 +1100

    now the app is launched when the widget is clicked

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/FootballScoreWidget.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/FootballScoreWidget.java
@@ -1,10 +1,13 @@
 package barqsoft.footballscores.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
+import barqsoft.footballscores.MainActivity;
 import barqsoft.footballscores.R;
 
 public class FootballScoreWidget extends AppWidgetProvider {
@@ -16,6 +19,7 @@ public class FootballScoreWidget extends AppWidgetProvider {
         int awayIconResourceId = R.drawable.liverpool;
         String score = "0 - 0";
 
+
         for (int widgetId : appWidgetIds) {
             int layoutId = R.layout.widget_list_item;
             RemoteViews views = new RemoteViews(context.getPackageName(), layoutId);
@@ -25,8 +29,11 @@ public class FootballScoreWidget extends AppWidgetProvider {
             views.setTextViewText(R.id.away_name, awayTeamName);
             views.setTextViewText(R.id.score_textview, score);
 
+            Intent appIntent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, appIntent, 0);
+            views.setOnClickPendingIntent(R.id.widget_root_layout, pendingIntent);
+
             appWidgetManager.updateAppWidget(widgetId, views);
         }
-
     }
 }
