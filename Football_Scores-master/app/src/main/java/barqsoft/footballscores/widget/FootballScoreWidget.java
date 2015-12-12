dee3f445c33f8a8c4a03ca757facd4de95afa2c0
Shamim Ahmed <shamim.buet.99@gmail.com>
2015-12-13 08:50:54 +1100

    some of the values on the widget are now shown correctly

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/FootballScoreWidget.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/FootballScoreWidget.java
@@ -3,10 +3,12 @@ package barqsoft.footballscores.widget;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.RemoteViews;
@@ -25,21 +27,12 @@ public class FootballScoreWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        String homeTeamName = "Arsenal";
-        String awayTeamName = "Liverpool";
-        int homeIconResourceId = R.drawable.arsenal;
-        int awayIconResourceId = R.drawable.liverpool;
-        String score = "0 - 0";
-
+        Log.i(TAG, "onUpdate method called ...");
 
         for (int widgetId : appWidgetIds) {
             int layoutId = R.layout.widget_list_item;
             RemoteViews views = new RemoteViews(context.getPackageName(), layoutId);
-            views.setImageViewResource(R.id.home_crest, homeIconResourceId);
-            views.setImageViewResource(R.id.away_crest, awayIconResourceId);
-            views.setTextViewText(R.id.home_name, homeTeamName);
-            views.setTextViewText(R.id.away_name, awayTeamName);
-            views.setTextViewText(R.id.score_textview, score);
+            updateView(context, views);
 
             Intent appIntent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, appIntent, 0);
@@ -54,14 +47,49 @@ public class FootballScoreWidget extends AppWidgetProvider {
         super.onReceive(context, intent);
 
         if (Constants.ACTION_DATA_UPDATED.equals(intent.getAction())) {
-            Log.i(TAG, "update event received ...");
-
-            SimpleDateFormat dateFormatter = new SimpleDateFormat(DATE_FORMAT);
-            String dateStr = dateFormatter.format(new Date());
-            Uri searchUri = DatabaseContract.scores_table.buildScoreWithDate();
-
-            Cursor cursor = context.getContentResolver().query(searchUri, null, null, new String[]{dateStr}, null);
-            cursor.close();
+            Log.i(TAG, "onReceive method called ...");
         }
+    }
+
+    private void updateView(Context context, RemoteViews views) {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat(DATE_FORMAT);
+        String dateStr = dateFormatter.format(new Date());
+        Uri searchUri = DatabaseContract.scores_table.buildScoreWithDate();
+
+        Cursor cursor = context.getContentResolver().query(searchUri, null, null, new String[]{dateStr}, null);
+
+        if (cursor.getCount() > 0) {
+            cursor.moveToFirst();
+            ContentValues values = readCursor(cursor);
+
+            String scoreStr = String.format("%s - %s",
+                    values.getAsString(DatabaseContract.scores_table.HOME_GOALS_COL),
+                    values.getAsString(DatabaseContract.scores_table.AWAY_GOALS_COL));
+            int homeIconResourceId = R.drawable.arsenal;
+            int awayIconResourceId = R.drawable.liverpool;
+
+            views.setImageViewResource(R.id.home_crest, homeIconResourceId);
+            views.setImageViewResource(R.id.away_crest, awayIconResourceId);
+            views.setTextViewText(R.id.home_name, values.getAsString(DatabaseContract.scores_table.HOME_COL));
+            views.setTextViewText(R.id.away_name, values.getAsString(DatabaseContract.scores_table.AWAY_COL));
+            views.setTextViewText(R.id.score_textview, scoreStr);
+        }
+
+        cursor.close();
+    }
+
+    private ContentValues readCursor(Cursor cursor) {
+        ContentValues values = new ContentValues();
+        int homeColumnIndex = cursor.getColumnIndex(DatabaseContract.scores_table.HOME_COL);
+        int awayColumnIndex = cursor.getColumnIndex(DatabaseContract.scores_table.AWAY_COL);
+        int homeGoalsColumnIndex = cursor.getColumnIndex(DatabaseContract.scores_table.HOME_GOALS_COL);
+        int awayGoalsColumnIndex = cursor.getColumnIndex(DatabaseContract.scores_table.AWAY_GOALS_COL);
+
+        values.put(DatabaseContract.scores_table.HOME_COL, cursor.getString(homeColumnIndex));
+        values.put(DatabaseContract.scores_table.AWAY_COL, cursor.getString(awayColumnIndex));
+        values.put(DatabaseContract.scores_table.HOME_GOALS_COL, cursor.getString(homeGoalsColumnIndex));
+        values.put(DatabaseContract.scores_table.AWAY_GOALS_COL, cursor.getString(awayGoalsColumnIndex));
+
+        return values;
     }
 }
