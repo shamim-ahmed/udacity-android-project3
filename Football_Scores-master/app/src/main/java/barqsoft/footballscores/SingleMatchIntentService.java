ddffca83b776c7cb83adbea83efaba4d620349a6
Shamim Ahmed <shamim.buet.99@gmail.com>
2015-12-28 17:52:12 +1100

    the simple widget does not pass any position to the app anymore. It simply brings the app to front

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/SingleMatchIntentService.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/SingleMatchIntentService.java
@@ -45,8 +45,7 @@ public class SingleMatchIntentService extends IntentService {
 
             // configure what happens when the widget is clicked
             Intent appIntent = new Intent(appContext, MainActivity.class);
-            appIntent.putExtra(Constants.SELECTED_INDEX_ATTRIBUTE, 0);
-            PendingIntent pendingIntent = PendingIntent.getActivity(appContext, 0, appIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            PendingIntent pendingIntent = PendingIntent.getActivity(appContext, 0, appIntent, 0);
             views.setOnClickPendingIntent(R.id.widget_root_layout, pendingIntent);
 
             appWidgetManager.updateAppWidget(widgetId, views);
