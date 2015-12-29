a98b0de2e22c29eaf9c967307ea3150641253130
Shamim Ahmed <shamim.buet.99@gmail.com>
2015-12-29 20:51:02 +1100

    fixed a potential NPE

--- alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -111,9 +111,14 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         ((TextView) rootView.findViewById(R.id.fullBookDesc)).setText(desc);
 
         String authors = data.getString(data.getColumnIndex(AlexandriaContract.AuthorEntry.AUTHOR));
-        String[] authorsArr = authors.split(Constants.COMMA_SEPARATOR);
-        ((TextView) rootView.findViewById(R.id.authors)).setLines(authorsArr.length);
-        ((TextView) rootView.findViewById(R.id.authors)).setText(authors.replace(Constants.COMMA_SEPARATOR, Constants.NEWLINE));
+
+        // fixed another potential NPE
+        if (authors != null) {
+            String[] authorsArr = authors.split(Constants.COMMA_SEPARATOR);
+            ((TextView) rootView.findViewById(R.id.authors)).setLines(authorsArr.length);
+            ((TextView) rootView.findViewById(R.id.authors)).setText(authors.replace(Constants.COMMA_SEPARATOR, Constants.NEWLINE));
+        }
+
         String imgUrl = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.IMAGE_URL));
         if(Patterns.WEB_URL.matcher(imgUrl).matches()){
             new DownloadImage((ImageView) rootView.findViewById(R.id.fullBookCover)).execute(imgUrl);
